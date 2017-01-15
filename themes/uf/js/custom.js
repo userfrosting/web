839fbf4480fecf608d5182195209d548900f1e07
SarahBaghdadi <sera.baghdadi@gmail.com>
2017-01-15 01:58:31 -0500

    Active nav li a

--- themes/uf/js/custom.js
+++ themes/uf/js/custom.js
@@ -1,3 +1,13 @@
   $(document).ready(function(){
-     $(".button-collapse").sideNav();
-  });
+    $(".button-collapse").sideNav(); //Materialize mobile Nav
+
+    var url = window.location;
+    var element = $('.navbar-fixed li a').filter(function() {
+        // Strip out everything after the hash, if present
+        var url_head = url.href.split('#', 1)[0];
+        return this.href ==url_head;  // || url.href.indexOf(this.href) == 0   // Include this if you want to color all parent URIs as well
+    }).addClass('active').parent().parent().addClass('in').parent();
+    if (element.is('li')) {
+        element.addClass('active');
+    }
+});
