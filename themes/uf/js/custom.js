aef185a9ee434d110fd7222a911610ab908bc58f
Sarah Baghdadi <sbaghdad@umail.iu.edu>
2017-01-30 01:46:14 -0500

    Bug fixing
    Instead of pagination, make the features sections two galleries

--- themes/uf/js/custom.js
+++ themes/uf/js/custom.js
@@ -15,7 +15,7 @@
         // Strip out everything after the hash, if present
         var url_head = url.href.split('#', 1)[0];
         return this.href ==url_head;  // || url.href.indexOf(this.href) == 0   // Include this if you want to color all parent URIs as well
-    }).addClass('active').parent().parent().addClass('in').parent();
+    }).addClass('nav-active').parent().parent().addClass('in').parent();
     if (element.is('li')) {
         element.addClass('active');
     }
