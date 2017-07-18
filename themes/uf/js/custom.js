392336c5239f20b8f19972f9e85e63adfb9b3034
Sarah Baghdadi <sbaghdad@umail.iu.edu>
2017-07-18 13:39:01 -0400

    Nav content

--- themes/uf/js/custom.js
+++ themes/uf/js/custom.js
@@ -1,43 +1,52 @@
-  $(document).ready(function(){
-    
-    //Materialize mobile Nav
-    $(".button-collapse").sideNav();
-    
-    //Materialize carousel      
-      $('#main-carousel').carousel();
-      
-      //$('#features-carousel').carousel({fullWidth: true});
+$(document).ready(function(){
+	
+	//Materialize mobile nav
+	$(".button-collapse").sideNav();
+	
+	//Materialize carousel      
+	  $('#main-carousel').carousel();
 
-
-    //Active Nav Li a
-    var url = window.location;
-    var element = $('.navbar-fixed li a').filter(function() {
-        // Strip out everything after the hash, if present
-        var url_head = url.href.split('#', 1)[0];
-        return this.href ==url_head;  // || url.href.indexOf(this.href) == 0   // Include this if you want to color all parent URIs as well
-    }).addClass('nav-active').parent().parent().addClass('in').parent();
-    if (element.is('li')) {
-        element.addClass('active');
+  //Materialize dropdown
+  $(".dropdown-button").dropdown({
+    hover: true, 
+    inDuration: 300,
+    outDuration: 225,
+    alignment: "left", // Aligns dropdown to left or right edge (works with constrain_width)
+    gutter: 0, // Spacing from edge
+    belowOrigin: true,
     }
-    
-    //Match Height
-    $(function() {
-      $('.hover-img').matchHeight({
-          target: $('.hc')
-      });
-    });
-    
-    //Match Width
-    $('.hover-img').matchWidth($('.hc'));
-    
-    //Material Box
-    $('.materialboxed').materialbox();
+  );
+  
+  //Materialize box
+	$('.materialboxed').materialbox();
+
+	//Active Nav Li a
+	var url = window.location;
+	var element = $('.navbar-fixed li a').filter(function() {
+		// Strip out everything after the hash, if present
+		var url_head = url.href.split('#', 1)[0];
+		return this.href ==url_head;  // || url.href.indexOf(this.href) == 0   // Include this if you want to color all parent URIs as well
+	}).addClass('nav-active').parent().parent().addClass('in').parent();
+	if (element.is('li')) {
+		element.addClass('active');
+	}
+	
+	//Match Height
+	$(function() {
+	  $('.hover-img').matchHeight({
+		  target: $('.hc')
+	  });
+	});
+	
+	//Match Width
+	$('.hover-img').matchWidth($('.hc'));
+	
 });
   
 $(document).ready(function(){
    $('.slick-slider').slick({
-      variableWidth: true,
-      centerMode: true,
+	  variableWidth: true,
+	  centerMode: true,
    });
  });
  
