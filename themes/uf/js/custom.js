f99ae5b41f237cead03e58b50f532088615117da
alexweissman <alex@alexanderweissman.com>
2017-11-28 13:19:03 -0500

    fix custom JS formatting

--- themes/uf/js/custom.js
+++ themes/uf/js/custom.js
@@ -1,35 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	//Materialize mobile nav
-	$(".button-collapse").sideNav();
-	//Materialize carousel
-	  $('#main-carousel').carousel();
+	$('.button-collapse').sideNav();
+
     //Materialize dropdown
-    $(".dropdown-button").dropdown({
+    $('.dropdown-button').dropdown({
       hover: true,
       inDuration: 300,
       outDuration: 225,
-      alignment: "left", // Aligns dropdown to left or right edge (works with constrain_width)
+      alignment: 'left', // Aligns dropdown to left or right edge (works with constrain_width)
       gutter: 0, // Spacing from edge
-      belowOrigin: true,
-    }
-  );
-    //Materialize box
-	$('.materialboxed').materialbox();
-	//Active Nav Li a
+      belowOrigin: true
+    });
+
+    //Active Nav Li a
 	var url = window.location;
 	var element = $('.navbar-fixed li a').filter(function() {
 		// Strip out everything after the hash, if present
 		var url_head = url.href.split('#', 1)[0];
-		return this.href ==url_head;  // || url.href.indexOf(this.href) == 0   // Include this if you want to color all parent URIs as well
+		return (this.href == url_head);  // || url.href.indexOf(this.href) == 0   // Include this if you want to color all parent URIs as well
 	}).addClass('nav-active').parent().parent().addClass('in').parent();
+
 	if (element.is('li')) {
 		element.addClass('active');
 	}
-});
 
-$(document).ready(function(){
-   $('.slick-slider').slick({
-	  variableWidth: true,
-	  centerMode: true,
-   });
- });
+    // Carousel for site gallery
+    $('.slick-slider').slick({
+       variableWidth: true,
+       centerMode: true
+    });
+});
